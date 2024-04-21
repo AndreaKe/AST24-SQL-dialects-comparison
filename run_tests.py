2b111659ce42363d0958e53bab13075198944e14
andreak <keuscha@ethz.ch>
2024-04-21 13:38:23 +0200

    refactor run script and add duckdb related code

--- run_tests.py
+++ run_tests.py
@@ -3,23 +3,186 @@ import sys, logging
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import re
+import duckdb
 
-logging.basicConfig(stream=sys.stderr, level=logging.WARNING) # change level to INFO or DEBUG to see more output
+logging.basicConfig(stream=sys.stderr, level=logging.ERROR) # change level to INFO or DEBUG to see more output
 
-DATABASE_NAME = "regression"
-DATABASE_USER = "postgres"
-HOST = "localhost"
-PORT = 5432
+class SQLDialectWrapper(object):
+    name = ""
+    db_conn = None
+    db_cursor = None
+    result_file = None
 
-DROP_DB_STMT = "DROP DATABASE IF EXISTS {}".format(DATABASE_NAME)
+    PASS = 1
+    ERROR = -1
 
-CREATE_DB_STMT = """CREATE DATABASE {} WITH 
-    OWNER = {} 
-    ENCODING = 'UTF8' 
-    TABLESPACE = pg_default 
-    LC_COLLATE = 'C' 
-    LC_CTYPE = 'en_US.UTF-8' 
-    TEMPLATE = template0 """.format(DATABASE_NAME, DATABASE_USER)
+    def __init__(self) -> None:
+        self.setup_clean_db()
+        self.setup_connection()
+
+    def setup_clean_db(self):
+        pass
+
+    def setup_connection(self):
+        pass
+
+    def teardown_connection(self):
+        pass
+
+    def open_result_file(self, result_folder):
+        pass
+
+    def write_to_result_file(self, content):
+        pass
+
+    def close_result_file(self):
+        pass
+
+    def exec_command(self, printErrors, command):
+        pass
+
+    def create_result_dict(self, status, result):
+        return {'status': status, 'result': result}
+    
+    def create_no_result_dict(self):
+        return self.create_result_dict(self.PASS, None)
+    
+    def create_error_dict(self):
+        return self.create_result_dict(self.ERROR, None)
+
+
+class DuckDB(SQLDialectWrapper):
+    name = "duckdb"
+    db_conn = None
+    db_cursor = None
+    result_file = None
+
+    RESULT_FILE_NAME = "duckdb_result.txt"
+
+    def setup_clean_db(self):
+        # nothing to do here because we work with in-memory database
+        pass
+
+    def setup_connection(self):
+        self.db_conn = duckdb.connect()
+
+    def teardown_connection(self):
+        pass
+
+    def open_result_file(self, result_folder):
+        self.result_file = open(os.path.join(result_folder, self.RESULT_FILE_NAME), 'w') 
+
+    def write_to_result_file(self, content):
+        self.result_file.write(content)
+
+    def close_result_file(self):
+        self.result_file.close()
+
+    def exec_command(self, printErrors, command):
+        try:
+            result = self.db_conn.sql(command)
+            if result == None:
+                return self.create_no_result_dict()
+            result = result.fetchall()
+            result_string = "RESULT: \n\t{}\n".format(result)
+            logging.info(result_string)
+            self.result_file.write(result_string)
+            return self.create_result_dict(self.PASS, result)
+        except Exception as e:
+            self.result_file.write("ERROR: {}\n{}\n".format(command, e))
+            if printErrors: #and not re.search(pattern, expected_results):
+                logging.warning("ERROR: {}\n{}\n".format(command, e))
+            return self.create_error_dict()
+
+
+class PostgreSQL(SQLDialectWrapper):
+    name = "postgres"
+    db_conn = None
+    db_cursor = None
+    result_file = None
+
+    RESULT_FILE_NAME = "postgres_result.txt"
+
+    DATABASE_NAME = "regression"
+    DATABASE_USER = "postgres"
+    HOST = "localhost"
+    PORT = 5432
+
+    DROP_DB_STMT = "DROP DATABASE IF EXISTS {}".format(DATABASE_NAME)
+
+    CREATE_DB_STMT = """CREATE DATABASE {} WITH 
+        OWNER = {} 
+        ENCODING = 'UTF8' 
+        TABLESPACE = pg_default 
+        LC_COLLATE = 'C' 
+        LC_CTYPE = 'en_US.UTF-8' 
+        TEMPLATE = template0 """.format(DATABASE_NAME, DATABASE_USER)
+
+    def setup_clean_db(self):
+        logging.debug("POSTGRESQL: Setup clean database {} with user {} at {}:{}".format(self.DATABASE_NAME, self.DATABASE_USER, self.HOST, self.PORT))
+        tmp_db_conn = psycopg2.connect(user=self.DATABASE_USER, password="*****", host=self.HOST, port=self.PORT)
+        tmp_db_conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        tmp_db_cursor = tmp_db_conn.cursor()
+        logging.debug(self.DROP_DB_STMT)
+        tmp_db_cursor.execute(self.DROP_DB_STMT)
+        tmp_db_conn.commit()
+        logging.debug(self.CREATE_DB_STMT)
+        tmp_db_cursor.execute(self.CREATE_DB_STMT)
+        tmp_db_conn.commit()
+        tmp_db_cursor.close()
+        tmp_db_conn.close()
+
+    def setup_connection(self):
+        logging.info("POSTGRESQL: Connecting to database {} at {}:{}".format(self.DATABASE_NAME, self.HOST, self.PORT))
+        self.db_conn = psycopg2.connect(user=self.DATABASE_USER, password="*****", host=self.HOST, port=self.PORT, database=self.DATABASE_NAME)
+        self.db_conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        self.db_cursor = self.db_conn.cursor()
+
+    def teardown_connection(self):
+        # postgres_cursor.execute(DROP_DB_STMT) -> need to close connection first
+        logging.info("POSTGRESQL: Close connection to database")
+        self.db_cursor.close()
+        self.db_conn.close()
+
+    def open_result_file(self, result_folder):
+        self.result_file = open(os.path.join(result_folder, self.RESULT_FILE_NAME), 'w') 
+
+    def write_to_result_file(self, content):
+        self.result_file.write(content)
+
+    def close_result_file(self):
+        self.result_file.close()
+
+    def exec_command(self, printErrors, command):
+        try:
+            self.db_cursor.execute(command)
+            result = self.db_cursor.fetchall() # TODO: here we can compare results to other DBMS results
+            result_string = "RESULT: \n\t{}\n".format(result)
+            logging.info(result_string)
+            self.result_file.write(result_string)
+            return self.create_result_dict(self.PASS, result)
+        except psycopg2.ProgrammingError as e:
+            if str(e) == "no results to fetch": 
+                return self.create_no_result_dict()
+            else:
+                self.result_file.write("ProgrammingError: {}\n{}\n".format(command, e))
+                        # pattern = r'.*{};[^\n]*\s*(ERROR)?:?\s*{}.*'.format(re.escape(command.strip()), re.escape(str(e)))
+                if printErrors: # and not re.search(pattern, expected_results):
+                    logging.warning("ProgrammingError: {}\n{}\n".format(command, e))
+                return self.create_error_dict()
+                
+        except psycopg2.Error as e:
+                    #pattern = r'.*{};?[^\n]*\s*(ERROR)?:?\s*{}.*'.format(re.escape(command).strip(), re.escape(str(e)))
+            self.result_file.write("ERROR: {}\n{}\n".format(command, e))
+            if printErrors: #and not re.search(pattern, expected_results):
+                logging.warning("ERROR: {}\n{}\n".format(command, e))
+            return self.create_error_dict()
+        except Exception as e:
+            self.result_file.write("ERROR: {}\n{}\n".format(command, e))
+            if printErrors:
+                logging.warning("ERROR: {}\n{}\n".format(command, e))
+            return self.create_error_dict()
+
 
 TEST_PATH = "postgres_tests"
 RESULT_PATH = "postgres_results"  
@@ -29,117 +192,82 @@ if(len(sys.argv) > 2):
     RESULT_PATH = str(sys.argv[2]) # 2. command line argument = RESULT_PATH
 
 
-def setup_clean_psql_db():
-    logging.debug("POSTGRESQL: Setup clean database {} with user {} at {}:{}".format(DATABASE_NAME, DATABASE_USER, HOST, PORT))
-    postgres_conn = psycopg2.connect(user=DATABASE_USER, password="*****", host=HOST, port=PORT)
-    postgres_conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    postgres_cursor = postgres_conn.cursor()
-    logging.debug(DROP_DB_STMT)
-    postgres_cursor.execute(DROP_DB_STMT)
-    logging.debug(CREATE_DB_STMT)
-    postgres_cursor.execute(CREATE_DB_STMT)
-    postgres_cursor.close()
-    postgres_conn.close()
-
-
-def setup_postgres_connection():
-    logging.info("POSTGRESQL: Connecting to database {} at {}:{}".format(DATABASE_NAME, HOST, PORT))
-    postgres_conn = psycopg2.connect(user=DATABASE_USER, password="*****", host=HOST, port=PORT, database=DATABASE_NAME)
-    postgres_conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    return postgres_conn
-
-
-def get_postgres_cursor(postgres_conn):
-    return postgres_conn.cursor()
-
-
-def teardown_postgres_connection(postgres_cursor, postgres_conn):
-    # postgres_cursor.execute(DROP_DB_STMT) -> need to close connection first
-    logging.info("POSTGRESQL: Close connection to database")
-    postgres_cursor.close()
-    postgres_conn.close()
-
-
-def execute_sql_file_postgres(postgres_cursor, sql_file, result_file, expected_results_file, printErrors=True):
-    logging.debug("POSTGRESQL: Executing {}".format(sql_file))
+def execute_sql_file(sql_dialects, sql_file, result_folder, printErrors=True):
+    logging.debug("Executing test case {}".format(sql_file))
     # We write the results to a file such that results can be compare without needing to rerun the tests all the time
-    result_file = open(result_file, 'w') 
+    for dialect in sql_dialects:
+        dialect.open_result_file(result_folder)
 
     test_file_stream = open(sql_file, 'r')
     test_file = test_file_stream.read()
     test_file_stream.close()
 
-    expected_results_stream = open(expected_results_file, 'r')
-    expected_results = expected_results_stream.read()
-    expected_results_stream.close()
+    # expected_results_stream = open(expected_results_file, 'r')
+    # expected_results = expected_results_stream.read()
+    # expected_results_stream.close()
 
-    sql_commands = re.sub(r'(--[^\n]*\n)', '', test_file) # remove comments
-    sql_commands = sql_commands.split(';') # here we get the individual sql commands!
+    #sql_commands = re.sub(r'(--[^\n]*\n)', '', test_file) # remove comments
+    sql_commands = test_file.split(';') # here we get the individual sql commands!
+    command_iter = iter(sql_commands)
     
-    i = 0
-    while i < len(sql_commands):
-        command = sql_commands[i]
-        dollar_count = command.count('$$')
-        double_quotes_count = command.count('"')
-        single_quotes_count = command.count("'")
+    while True:
+        try:
+            command = get_command(command_iter)
 
-        while dollar_count % 2 != 0 or  double_quotes_count % 2 != 0 or single_quotes_count % 2 != 0:
-            i = i + 1
-            command = command + ";" + sql_commands[i]
+            for dialect in sql_dialects:
+                dialect.write_to_result_file(command)
+            if command.strip() != '':
+                logging.info("\nExecuting command:  {}".format(command))
+                results = []
+                for dialect in sql_dialects:
+                    results.append(dialect.exec_command(printErrors, command))
+                # TODO compare results
+        except StopIteration:
+            break
+
+    for dialect in sql_dialects:
+        dialect.close_result_file()
+
+def get_command(command_iter):
+    command = next(command_iter)
+    dollar_count = command.count('$$')
+    double_quotes_count = command.count('"')
+    single_quotes_count = command.count("'")
+
+    while dollar_count % 2 != 0 or  double_quotes_count % 2 != 0 or single_quotes_count % 2 != 0:
+        try:
+            command = command + ";" + next(command_iter)
             dollar_count = command.count('$$')
             double_quotes_count = command.count('"')
             single_quotes_count = command.count("'")
+        except StopIteration as e:
+            logging.error("Index out of bounds. Command: ".format(command))
+            raise e
 
-        command_string = "{}\n".format(command)
-
-
-        logging.info("\n\tInfo: i={}, command={}".format(i, command_string))
-        result_file.write(command_string)
-        if command.strip() != '':
-            try:
-                postgres_cursor.execute(command)
-                result = postgres_cursor.fetchall() # TODO: here we can compare results to other DBMS results
-                result_string = "RESULT: \n\t{}\n".format(result)
-                logging.info(result_string)
-                result_file.write(result_string)
-            except psycopg2.ProgrammingError as e:
-                if str(e) != "no results to fetch": 
-                    pattern = r'.*{};[^\n]*\s*(ERROR)?:?\s*{}.*'.format(re.escape(command.strip()), re.escape(str(e)))
-                    if printErrors and not re.search(pattern, expected_results):
-                        logging.warning("ProgrammingError: {}\n{}\n".format(command, e))
-                        result_file.write("ProgrammingError: {}\n{}\n".format(command, e))
-            except psycopg2.Error as e:
-                pattern = r'.*{};?[^\n]*\s*(ERROR)?:?\s*{}.*'.format(re.escape(command).strip(), re.escape(str(e)))
-                if printErrors and not re.search(pattern, expected_results):
-                    logging.warning("ERROR: {}\n{}\n".format(command, e))
-                    result_file.write("ERROR: {}\n{}\n".format(command, e))
-        i = i + 1
-    result_file.close()
-
-def execute_single_test_postgres(test_folder, result_folder):
-    # we always want to drop and recreate the database because some tests might modify the data
-    logging.debug("POSTGRESQL: execute single test {} and storing results in {}\n".format(test_folder, result_folder))
-    setup_clean_psql_db()
-
-    postgres_conn = setup_postgres_connection()
-    postgres_cursor = get_postgres_cursor(postgres_conn)
-
-    execute_sql_file_postgres(postgres_cursor, test_folder + "/setup.sql", os.path.join(result_folder, "postgres_setup.txt"), os.path.join(test_folder, 'result.txt'), False) 
-    execute_sql_file_postgres(postgres_cursor, test_folder + "/test.sql", os.path.join(result_folder, "postgres_results.txt"), os.path.join(test_folder, 'result.txt'))
-
-    teardown_postgres_connection(postgres_cursor, postgres_conn)
+    return "{}\n".format(command)
 
 
 def execute_single_test(test_folder, result_folder):
+    if not os.path.exists(result_folder):
+        os.makedirs(result_folder)
+
     print("==============================================\n")
     print("execute single test {} and storing results in {}\n".format(test_folder, result_folder))
-    execute_single_test_postgres(test_folder, result_folder)
-    # TODO: execute tests on DuckDB and MySQL, compare and report the results
-    # Note: probably we want to setup 3 databases first (for Postgres, DuckDB, MySQL), 
-    # then execute setup for all of them
-    # then execute test.sql one statement after the other for all of them.
-    # With this structure we can compare the results of each statement directly.
+    # we always want to drop and recreate the database because some tests might modify the data
 
+    dialects = [PostgreSQL(), DuckDB()] # TODO add MySQL
+
+    execute_sql_file(dialects, test_folder + "/setup.sql", result_folder, False) 
+
+    for dialect in dialects:
+        if dialect.name == "postgres":
+            dialect.db_cursor.execute("SELECT pg_catalog.set_config('search_path', 'public', false);")
+        # TODO check if we have to do something for MySQL, DuckDB
+
+    execute_sql_file(dialects, test_folder + "/test.sql", result_folder)
+
+    for dialect in dialects:
+        dialect.teardown_connection()
 
 
 def execute_tests_in_folder_rec(test_folder, result_folder):
