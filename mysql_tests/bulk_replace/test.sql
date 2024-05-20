aa4e52a1b6e64fde5638dd52f8abec3855f152af
ruh ste <ler.steffi@gmail.com>
2024-05-20 12:58:55 +0200

    Add 3 mySQL tests

---
+++ mysql_tests/bulk_replace/test.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b2fb8ba0f8f3fe7c88b7f2092aac484404413664c350cfffdc984f792226a36f
+size 211
---
+++ mysql_tests/packet/test.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e4dd7727b3d2dcd481e857084efb2005972ecbfb2ad2b3881db4fa2f68348ee
+size 3640
